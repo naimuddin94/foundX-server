f22c286669484f221e9cbfdcb1bf14bcd7616d12
naimuddin94 <developernaimuddin@gmail.com>
2024-09-11 00:00:25 +0600

    add comment on multer file

--- src/app/config/multer.config.ts
+++ src/app/config/multer.config.ts
@@ -2,21 +2,26 @@ import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import { cloudinaryUpload } from './cloudinary.config';
 
+// Helper function to remove the file extension from the filename
 const removeExtension = (filename: string) => {
   return filename.split('.').slice(0, -1).join('.');
 };
 
+// Configure Cloudinary storage for multer
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinaryUpload,
+  cloudinary: cloudinaryUpload, // Cloudinary instance from the configuration
   params: {
+    // Define the public ID format for the uploaded file
     public_id: (_req, file) =>
-      Math.random().toString(36).substring(2) +
+      Math.random().toString(36).substring(2) + // Generate a random string
       '-' +
-      Date.now() +
+      Date.now() + // Append the current timestamp
       '-' +
-      file.fieldname +
+      file.fieldname + // Append the field name
       '-' +
-      removeExtension(file.originalname),
+      removeExtension(file.originalname), // Remove file extension and append the original name
   },
 });
+
+// Export the configured multer upload function
 export const multerUpload = multer({ storage: storage });
